d4cd2cde79ab1c06a8ce4d0893746777127a884e
Eric <eric.swotinsky@gmail.com>
2018-03-19 11:40:30 -0700

    add initial main.js functionality; update index.html.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,43 @@
+import {tamagotchi} from './tamagotchi.js'
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 
+$(function(){
+  $('#create-form').submit(function(event){
+    event.preventDefault();
+    $('#create').hide();
+    $('#display').show();
+
+    let name = $('#enter-name').val();
+    let myTamagotchi = new tamagotchi(name);
+    $('#tamagotchi-name').text(myTamagotchi.name);
+
+    myTamagotchi.timePasses();
+
+    setInterval(function(){
+
+      $('#food-level').text(myTamagotchi.foodLevel);
+      $('#play-level').text(myTamagotchi.playLevel);
+      $('#sleep-level').text(myTamagotchi.sleepLevel);
+
+    }, 1000);
+
+    $('#food-button').click(function(){
+      myTamagotchi.feed();
+      $('#food-level').text(myTamagotchi.foodLevel);
+    })
+
+    $('#play-button').click(function(){
+      myTamagotchi.play();
+      $('#food-level').text(myTamagotchi.playLevel);
+    })
+
+    $('#sleep-button').click(function(){
+      myTamagotchi.sleep();
+      $('#food-level').text(myTamagotchi.sleepLevel);
+    })
+
+  })
+});
